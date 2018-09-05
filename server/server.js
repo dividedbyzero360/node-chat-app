10a8c5cc126c116679ce73de8ea705976513aeaf
Wastabir Rajvee Alam <wastabirrajveealamsa_cse10@its.edu.in>
2018-09-05 03:51:39 -0400

    Added connect and disconnect event handlers for websockets

--- server/server.js
+++ server/server.js
@@ -1,15 +1,24 @@
 const path=require("path");
 const express=require("express");
-
+const socketIO=require("socket.io");
+const http=require("http");
 const app=express();
+
+const server=http.createServer(app);
+var io=socketIO(server);
 
 app.use(express.static(path.join(__dirname,"/../public")))
 
+io.on("connection",(socket)=>{
+  console.log("New user connected");
+});
+
+io.on("disconnect",(socket)=>{
+    console.log("User disconnected");
+});
 
 const port=process.env.Port || 3000;
-app.listen(port,()=>{
+server.listen(port,()=>{
     console.log(`Server started at ${port}`);
 })
 
-
-//console.log(path.join(__dirname,"/../public"));
