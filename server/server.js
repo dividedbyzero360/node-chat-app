3c3cc3d83e9cb1d21cf15441db5d4d07990b0be9
Wastabir Rajvee Alam <wastabirrajveealamsa_cse10@its.edu.in>
2018-09-07 15:26:22 -0400

    Styled the page

--- server/server.js
+++ server/server.js
@@ -3,36 +3,27 @@ const express=require("express");
 const socketIO=require("socket.io");
 const http=require("http");
 const app=express();
-
+const {generateMessage,generateLocationMessage} = require('./utils/message');
 const server=http.createServer(app);
 var io=socketIO(server);
 app.use(express.static(path.join(__dirname,"/../public")));
 io.on("connection",(socket)=>{
   console.log("New user connected");
-  socket.emit("newMessage",{
-    from:"Admin",
-    text:"Welcome to chat app",
-    createdAt: new Date().getTime()
-  });
-  socket.broadcast.emit("newMessage",{
-    from:"Admin",
-    text:"New user joined to the chat",
-    createdAt: new Date().getTime()
-  });
-  socket.on("createMessage",function(message){
+  socket.emit("newMessage",generateMessage('Admin', 'Welcome to the chat app'));
+  socket.broadcast.emit("newMessage",generateMessage('Admin', 'New user joined'));
+  socket.on("createMessage",function(message,callback){
    console.log("Create Message", message);
-   io.emit("newMessage",{
-    from:  message.from,
-    text: message.text,
-    createdAt: new Date().getTime()
-   });
+   io.emit("newMessage", generateMessage(message.from, message.text));
+   callback("Ok from server");
 // socket.broadcast.emit("newMessage",{
 //     from:  message.from,
 //     text: message.text,
 //     createdAt: new Date().getTime()
 // });
   });
-
+  socket.on('createLocationMessage', (coords) => {
+    io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
+  });
   socket.on("disconnect",()=>{
     console.log("User was disconnected");
   });
